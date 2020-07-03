f58a5850c4a65e1b135ed000376ead6ef41aefa0
Daniel Ponce <dmpb019@gmail.com>
2020-07-02 22:18:33 -0500

    three master

--- Collection.php
+++ Collection.php
@@ -6,28 +6,9 @@ class Collection
     public $items = [];
 
 
-    public function __construct(array $items)
+    public function threeMaster()
     {
-        $this->items = $items;
-    }
-
-    public function all()
-    {
-        return $this->items;
-    }
-
-    public function soa()
-    {
-        return 'soa';
-    }
-
-    public function concat($items)
-    {
-        $result = new static($this->items);
-
-        array_push($result->items, $items);
-
-        return $result;
+        return 'three';
     }
 
     
