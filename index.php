eaa6e20e485fc29e1aed81e2d83bb0a7a956e767
Daniel Ponce <dmpb019@gmail.com>
2020-07-02 22:33:28 -0500

    Six Commit

--- index.php
+++ index.php
@@ -3,12 +3,3 @@
 require "helpers.php";
 
 
-$collection = collect(["daniel", "david"]);
-
-$push = $collection->concat("jessica")->concat("ponce")->all();
-
-echo "<pre>";
-
-var_dump($push);
-
-echo "</pre>";
