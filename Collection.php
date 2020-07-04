29d98b5818b25dcfcd8643541ea73abd74256a52
Daniel Ponce <dmpb019@gmail.com>
2020-07-03 20:30:30 -0500

    init 1

--- Collection.php
+++ Collection.php
@@ -3,63 +3,14 @@
 class Collection
 {
 
-    public $items = [];
-
-
-    public function fo1()
-    {
-        return 'fo1';
-    }
-
-    public function master2()
-    {
-        return '2';
-    }
-
-    public function master3()
-    {
-        return '3';
-    }
-
-    public function hio1()
-    {
-        return 'hio1';
-    }
-
-    public function hio2()
-    {
-        return 'hio2';
-    }
-
-    public function hio3()
-    {
-        return 'hio1';
-    }
-
-    public function hio5()
-    {
-        return 'hio2';
-    }
-
-    public function fetch()
-    {
-        return 'fetch';
-    }
-
-    public function fetch2()
+    public function new1()
     {
         
     }
 
-    public function fetch3()
+    public function new2()
     {
         
     }
-
-    public function fetch4()
-    {
-        
-    }
-
 
 }
